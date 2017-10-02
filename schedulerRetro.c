8068a4e7596c81df64b0d4ed2969e085f646db98
kireck211 <erickdesantiago11@gmail.com>
2017-10-01 19:51:18 -0500

    retro

--- schedulerRetro.c
+++ schedulerRetro.c
@@ -13,6 +13,7 @@ QUEUE *actual;
 QUEUE *last;
 
 int queues;
+int only_one;
 
 /*QUEUE* first_not_empty()
 {
@@ -26,10 +27,11 @@ int queues;
 void initialize()
 {
 	if (ready == NULL) {
-		ready = (QUEUE *)malloc(sizeof(QUEUE));
+		ready = (QUEUE *)malloc(sizeof(QUEUE) * 10000);
 		actual = &ready[0];
 		last = &ready[0];
 		queues = 1;
+		only_one = 1;
 	}
 }
 
@@ -46,69 +48,77 @@ void scheduler(int arguments)
 	if(event==NEWTHREAD)
 	{
 		// Un nuevo hilo va a la cola de listos
-		printf("NEWTHREAD-inicio\n");
+		//printf("NEWTHREAD-inicio\n");
+		only_one = 0;
 		actual = &ready[0];
 		threads[callingthread].status=READY;
 		_enqueue(actual,callingthread);		//Encola el nuevo hilo en la cola 0
-		printf("NEWTHREAD-fin\n");
+		//printf("NEWTHREAD-fin\n");
 	}
 
 	if(event==TIMER)
 	{
-		printf("TIMER-inicio\n");
+		//printf("TIMER-inicio\n");
 		if (actual == last) { // Agregar nueva cola
 			queues++;
-			ready = (QUEUE *)realloc(ready, sizeof(QUEUE) * queues); // Crea espacio para nueva cola
+			//ready = (QUEUE *)realloc(ready, sizeof(QUEUE) * (queues + 1)); // Crea espacio para nueva cola
 			last = &ready[queues -1];
 		}
 
 		threads[callingthread].status=READY;
-		_enqueue(&(*(actual+1)),callingthread);
+		//printf("Aqui llega\n");
+
+		if (only_one)
+			_enqueue(&(*actual), callingthread);
+		else
+			_enqueue(&(*(actual+1)),callingthread);
 		//if (first_not_empty() != NULL)
 		changethread=1;
-		printf("TIMER-fin\n");
+		//printf("TIMER-fin\n");
 	}
 	
 	if(event==BLOCKTHREAD)
 	{
-		printf("BLOCKTHREAD-inicio\n");
+		//printf("BLOCKTHREAD-inicio\n");
 		threads[callingthread].status=BLOCKED;
 		_enqueue(&waitinginevent[blockevent],callingthread);
 
 		changethread=1;
-		printf("BLOCKTHREAD-fin\n");
+		//printf("BLOCKTHREAD-fin\n");
 	}
 
 	if(event==ENDTHREAD)
 	{
-		printf("ENDTHREAD-inicio\n");
+		//printf("ENDTHREAD-inicio\n");
 		threads[callingthread].status=END;
 		changethread=1;
-		printf("ENDTHREAD-fin\n");
+		//printf("ENDTHREAD-fin\n");
 	}
 	
 	if(event==UNBLOCKTHREAD)
 	{
-			printf("UNBLOCKTHREAD-inicio\n");
+			//printf("UNBLOCKTHREAD-inicio\n");
 			actual = &ready[0];
 			threads[callingthread].status=READY;
 			_enqueue(actual,callingthread);
-			printf("UNBLOCKTHREAD-fin\n");
+			//printf("UNBLOCKTHREAD-fin\n");
 	}
 
 	
 	if(changethread)
 	{
-		printf("changethread-inicio\n");
+		//printf("changethread-inicio\n");
 		//actual = first_not_empty();
 		old=currthread;
 		next=_dequeue(actual);
+		//printf("old:%d\n", old);
+		//printf("next:%d\n", next);
 		if (_emptyq(actual))
 			actual = &(*(actual + 1));
 		
 		threads[next].status=RUNNING;
 		_swapthreads(old,next);
-		printf("changethread-fin\n");
+		//printf("changethread-fin\n");
 	}
 
 }
